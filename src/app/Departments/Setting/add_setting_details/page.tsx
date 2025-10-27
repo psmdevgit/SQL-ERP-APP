8ee0b93240fdca3ebea58fa263d16bae8c3f6f14
psmdevgit <mis@pothysswarnamahal.com>
2025-10-27 10:35:48 +0530

    local 2 serv

--- src/app/Departments/Setting/add_setting_details/page.tsx
+++ src/app/Departments/Setting/add_setting_details/page.tsx
@@ -39,7 +39,7 @@ export default function AddSettingDetails() {
   const router = useRouter();
 
   
-const apiBaseUrl = "https://erp-server-r9wh.onrender.com"; 
+const apiBaseUrl = "https://kalash.app"; 
 
 
 
