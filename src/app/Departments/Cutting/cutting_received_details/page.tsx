f78ac7e08dcbb82567e416212fdc39313a4b3032
psmdevgit <mis@pothysswarnamahal.com>
2025-10-27 10:45:16 +0530

    Change API base URL for cutting received details

--- src/app/Departments/Cutting/cutting_received_details/page.tsx
+++ src/app/Departments/Cutting/cutting_received_details/page.tsx
@@ -7,7 +7,9 @@ import { Button } from "@/components/ui/button";
 import { z } from 'zod';
 import { Label } from "@/components/ui/label";
 
-const apiBaseUrl = "https://erp-server-r9wh.onrender.com"; 
+// const apiBaseUrl = "https://erp-server-r9wh.onrender.com"; 
+
+const apiBaseUrl = "https://kalash.app";
 
 interface Cutting {
   Id: string;
