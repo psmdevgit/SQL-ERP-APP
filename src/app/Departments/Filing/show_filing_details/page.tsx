2c1c7cfebe564049d4c94f045d28ee56730186a2
psmdevgit <mis@pothysswarnamahal.com>
2025-10-27 10:52:20 +0530

    Update page.tsx

--- src/app/Departments/Filing/show_filing_details/page.tsx
+++ src/app/Departments/Filing/show_filing_details/page.tsx
@@ -70,7 +70,10 @@ const FilingDetailsPage = () => {
   const filingId = searchParams.get('filingId');
 
   
-const apiBaseUrl = "https://erp-server-r9wh.onrender.com"; 
+// const apiBaseUrl = "https://erp-server-r9wh.onrender.com"; 
+
+  
+const apiBaseUrl = "https://kalash.app";
 
   useEffect(() => {
     const fetchDetails = async () => {
