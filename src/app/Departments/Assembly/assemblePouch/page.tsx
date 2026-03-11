1fa37bc2574327d56b6a77d4440aad9f74fe012d
psmdevgit <mis@pothysswarnamahal.com>
2026-03-11 15:24:06 +0530

    all Changes

--- src/app/Departments/Assembly/assemblePouch/page.tsx
+++ src/app/Departments/Assembly/assemblePouch/page.tsx
@@ -11,9 +11,9 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@
 // -----------------------------
 // Configuration
 // -----------------------------
-// const apiBaseUrl = "http://localhost:4001"; // change to 
+const apiBaseUrl = "http://localhost:4001"; // change to 
 
-const apiBaseUrl ="https://kalash.app"
+//const apiBaseUrl ="https://kalash.app"
 
 // -----------------------------
 // Types (partial, flexible to tolerate column name variations)
